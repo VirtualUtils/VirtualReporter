7f0d31f3bac0aaeef45f11e484702907907ef23b
Russ Shearer <russ.shearer@gmail.com>
2011-12-19 23:24:12 -0500

    Fixed null object error due to missing VIEvent variable

--- modules/B05_ShowRemoved.ps1
+++ modules/B05_ShowRemoved.ps1
@@ -4,6 +4,8 @@ function ShowRemoved () {
   if ($ShowRemoved) {
 
     Write-CustomOut "..Checking for removed VMs"
+    
+    $VIEvent = Get-VIEvent -maxsamples 10000 -Start ($Date).AddDays(-$VMsNewRemovedAge)
     $OutputRemovedVMs = @($VIEvent | where {$_.Gettype().Name -eq "VmRemovedEvent"}| Select createdTime, @{N="User";E={(Find-Username (($_.userName.split("\"))[1])).Properties.displayname}}, fullFormattedMessage)
 
     If (($OutputRemovedVMs | Measure-Object).count -gt 0 -or $ShowAllHeaders) {
